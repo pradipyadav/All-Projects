6547013490178a14e8eb7ecab296a1dc89fbe25d
sony <yadav.pradip1995@gmail.com>
2018-04-17 20:42:15 +0530

    Simple Value Resources

--- asstsmlvlersrskotlin/src/main/java/com/example/asstsmlvlersrskotlin/MainActivity.kt
+++ asstsmlvlersrskotlin/src/main/java/com/example/asstsmlvlersrskotlin/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wdth=resources.getDimension(R.dimen.btnWdth)
+        val stng=resources.getString(R.string.btnGo)
+        val color=resources.getColor(R.color.btncol)
     }
 }
