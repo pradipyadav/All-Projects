91ae80331d21f5a62e8435071f8574911076b1e9
sony <yadav.pradip1995@gmail.com>
2018-05-09 22:12:11 +0530

    Notification and KeepNotes

---
+++ statusbarkotlin/src/main/java/com/example/statusbarkotlin/ntfnActiviy.kt
@@ -0,0 +1,12 @@
+package com.example.statusbarkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NtfnActiviy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ntfn_activiy)
+    }
+}
